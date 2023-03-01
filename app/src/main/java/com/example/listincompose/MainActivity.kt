e9dae9b2b6d896df9fb66ea0e61e1877e3b80255
Flymetwothemoon <2113203787@qq.com>
2023-03-01 14:38:56 +0800

    状态

--- app/src/main/java/com/example/listincompose/MainActivity.kt
+++ app/src/main/java/com/example/listincompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,10 +13,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -81,14 +79,16 @@ fun MessageListItem(message: Message) {
                 )
                 .size(100.dp) )
         Spacer(modifier = Modifier.width(30.dp))
-     
-        Column (){
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+        Column (modifier = Modifier.clickable{isExpanded=!isExpanded}){
             Text(
                 text = message.content,
                 style = MaterialTheme.typography.body2,
                 fontSize = 30.sp,
-                maxLines = 1,
-                color = MaterialTheme.colors.secondary
+                maxLines = if(isExpanded)Int.MAX_VALUE else 1,
+                color = if(isExpanded)MaterialTheme.colors.secondary else MaterialTheme.colors.onPrimary
            )
             Spacer(modifier = Modifier.height(30.dp))
             Text(text = message.sender,
