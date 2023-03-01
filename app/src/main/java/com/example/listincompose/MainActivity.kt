440a7e314a5ad6f60d6e600ccfc28862d6780332
Flymetwothemoon <2113203787@qq.com>
2023-03-01 14:07:57 +0800

    列表

---
+++ app/src/main/java/com/example/listincompose/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.listincompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.listincompose.ui.theme.ListInComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ListInComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Gray
+                ) {
+                    val messages = listOf(
+                        Message("Alice", "Hello, Bob!"),
+                        Message("Bob", "Hi, Alice!"),
+                        Message("Charlie", "What's up, guys?"),
+                        Message("Alice", "Hello, Bob!"),
+                        Message("Bob", "Hi, Alice!"),
+                        Message("Charlie", "What's up, guys?"),
+                        Message("Alice", "Hello, Bob!"),
+                        Message("Bob", "Hi, Alice!"),
+                        Message("Charlie", "What's up, guys?"),
+                        Message("Alice", "Hello, Bob!"),
+                        Message("Bob", "Hi, Alice!"),
+                        Message("Charlie", "What's up, guys?")
+                    )
+
+                    MessageList(messages = messages)
+                }
+            }
+        }
+    }
+}
+
+data class Message(val sender: String, val content: String)
+
+@Composable
+fun MessageList(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageListItem(message)
+        }
+    }
+}
+
+@Composable
+fun MessageListItem(message: Message) {
+    Row(modifier = Modifier
+        .background(MaterialTheme.colors.background)
+        .padding(all = 8.dp)) {
+        Image(painter = painterResource(id = R.mipmap.one),
+            contentDescription = null,
+            modifier = Modifier
+                .clip(
+                    CircleShape
+                )
+                .size(100.dp) )
+        Spacer(modifier = Modifier.width(30.dp))
+     
+        Column (){
+            Text(
+                text = message.content,
+                style = MaterialTheme.typography.body2,
+                fontSize = 30.sp,
+                maxLines = 1,
+                color = MaterialTheme.colors.secondary
+           )
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(text = message.sender,
+            fontSize = 30.sp)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/listincompose/ui/theme/Theme.kt
@@ -0,0 +1,49 @@
+package com.example.listincompose.ui.theme
+
+import android.graphics.Color
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+    background = androidx.compose.ui.graphics.Color.Blue,
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+    background = androidx.compose.ui.graphics.Color.Red,
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun ListInComposeTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
